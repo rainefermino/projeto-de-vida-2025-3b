bc551023cdc073fc385d32e5be0c4ef03b1819fe
jheni.fermino <163904723+rainefermino@users.noreply.github.com>
2025-06-11 11:45:42 -0300

    11-06

--- main.js
+++ main.js
@@ -31,14 +31,7 @@ function calculaTempo(tempoObjetivo) {
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0) {
-        return [dias, horas, minutos, segundos];
-    } else {
-        return [0, 0, 0, 0];
-    }
+    
 }
 
 function atualizaCronometro() {
